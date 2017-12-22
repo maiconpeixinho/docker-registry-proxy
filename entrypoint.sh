5f2b10d0f68c6c9fd014965b92931760b50e2f5c
Maicon Peixinho <maiconpeixinho@globo.com>
2017-12-22 15:41:09 -0200

    Fixing Linux Panic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 sed -i "s|{{REGISTRY_HOST}}|$REGISTRY_HOST|;s|{{PROXY_TIMEOUT}}|$PROXY_TIMEOUT|;s|{{REGISTRY_PORT}}|$REGISTRY_PORT|;s|{{SERVER_NAME}}|$SERVER_NAME|" /etc/nginx/conf.d/default.conf
 exec "$@"
